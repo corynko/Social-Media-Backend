75286e39d17efc3858da94cd30de3e059c774a19
corynko <ethankotel@gmail.com>
2023-07-18 16:55:33 -0600

    created file architecture, fleshed out models, built indexes, started routes

---
+++ models/Thoughts.js
@@ -0,0 +1,70 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Schema.Types.ObjectId(),
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => `My Formatted Date ${timestamp}`,
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => `My Formatted Date ${timestamp}`,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
